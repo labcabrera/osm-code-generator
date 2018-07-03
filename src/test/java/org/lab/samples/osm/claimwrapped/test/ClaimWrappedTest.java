7ae38aa1bbe2459ace4b3a88e872fab9ba48e1a2
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2018-07-03 11:36:12 +0100

    Wrapped claim service test

--- src/test/java/org/lab/samples/osm/claimwrapped/test/ClaimWrappedTest.java
+++ src/test/java/org/lab/samples/osm/claimwrapped/test/ClaimWrappedTest.java
@@ -6,6 +6,7 @@ import java.util.Calendar;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.lab.samples.osm.claimwrapped.model.OCorresExpdteS;
 import org.lab.samples.osm.claimwrapped.model.ODescripcionS;
 import org.lab.samples.osm.claimwrapped.model.OSinAccInS;
 import org.lab.samples.osm.claimwrapped.model.OSinAccOutS;
@@ -38,8 +39,22 @@ public class ClaimWrappedTest {
 		request.setHoraOcurrencia("00:00");
 
 		request.setDescripcion(new ArrayList<>());
-		request.getDescripcion().add(ODescripcionS.builder().descripcion("TEST DESC").build());
+		request.getDescripcion().add(ODescripcionS.builder().descripcion("Desc 01").build());
+		request.getDescripcion().add(ODescripcionS.builder().descripcion("Desc 02").build());
 
+		request.setCorrespondencia(new OCorresExpdteS());
+		request.getCorrespondencia().setTipoVia("XXX");
+		request.getCorrespondencia().setNomDest("Some streed name");
+		request.getCorrespondencia().setNumero("XXXX");
+		request.getCorrespondencia().setPortal("XX");
+		request.getCorrespondencia().setEsc("XXX");
+		request.getCorrespondencia().setPiso("XX");
+		request.getCorrespondencia().setPuerta("XX");
+		request.getCorrespondencia().setLocalidad("Madrid");
+		request.getCorrespondencia().setCodPostal("28001");
+		request.getCorrespondencia().setCodPais("ESP");
+		request.getCorrespondencia().setApartado("Madrid");
+		request.getCorrespondencia().setNomDest("Letter destination name");
 		return request;
 	}
 
